e7e20892ba5d9c6a3db321cd7b9b65f6a7913ca1
changyuchun <changyuchunttt@163.com>
2015-05-27 10:55:26 +0800

    修改

---
+++ MyTestTcp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCPLibrary.DefaultImplements;
+
+namespace MyTestTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var   _client = new BaseClientSocket();
+        }
+    }
+}
